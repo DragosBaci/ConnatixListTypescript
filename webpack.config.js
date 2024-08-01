da154664b8da9f7d55cd27e1a09e07c4cfc0a4bc
DragosBaci <dragos6171@gmail.com>
2024-08-02 01:20:31 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        hot: false,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+    optimization: {
+        minimize: true,
+    },
+};
